1bd51836b4295dc464596a8fded9bff2274cbab4
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-05 14:43:32 +0200

    add route

--- src/main/java/com/katdev/studentsystem/controller/StudentController.java
+++ src/main/java/com/katdev/studentsystem/controller/StudentController.java
@@ -1,7 +1,10 @@
 package com.katdev.studentsystem.controller;
 
+import com.katdev.studentsystem.model.Student;
 import com.katdev.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @Autowired
     private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
 }
