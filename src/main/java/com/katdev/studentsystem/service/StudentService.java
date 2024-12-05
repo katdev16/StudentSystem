a113c0bff77a99c4b3060b25a5a3579ecf0ab595
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-05 19:24:44 +0200

    backend complete

--- src/main/java/com/katdev/studentsystem/service/StudentService.java
+++ src/main/java/com/katdev/studentsystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.katdev.studentsystem.service;
 
 import com.katdev.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
--- src/main/java/com/katdev/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/katdev/studentsystem/service/StudentServiceImpl.java
@@ -4,10 +4,20 @@ import com.katdev.studentsystem.model.Student;
 import com.katdev.studentsystem.repository.StudentRepository;
 import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService{
     @Autowired
     private StudentRepository studentRepository;
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
     @Override
     public Student saveStudent(Student student) {
         return studentRepository.save(student);
