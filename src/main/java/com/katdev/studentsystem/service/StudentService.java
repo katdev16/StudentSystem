1a022f8d026a8f3c33beaacdf01cb56ca846b17f
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-05 14:40:00 +0200

    postmapping saving data into database

--- src/main/java/com/katdev/studentsystem/service/StudentService.java
+++ src/main/java/com/katdev/studentsystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.katdev.studentsystem.service;
 
+import com.katdev.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
--- src/main/java/com/katdev/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/katdev/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,15 @@
 package com.katdev.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.katdev.studentsystem.model.Student;
+import com.katdev.studentsystem.repository.StudentRepository;
+import jakarta.persistence.Access;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentServiceImpl implements StudentService{
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
