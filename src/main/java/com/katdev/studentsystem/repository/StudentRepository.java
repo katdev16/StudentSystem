065b38e0637ad1681bd43bb7d5e7991631e17e61
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-05 14:18:53 +0200

    springboot configuration with Mysql

--- src/main/java/com/katdev/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/katdev/studentsystem/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.katdev.studentsystem.repository;
 
-public interface StudentRepository {
+import com.katdev.studentsystem.model.Student;
+import jdk.jfr.Registered;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
