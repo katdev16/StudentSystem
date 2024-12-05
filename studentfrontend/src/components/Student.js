6554e8851062bc3f9e763823ebba2a82cb4f1a48
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-05 21:51:50 +0200

    usestate for form

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -6,17 +6,24 @@ import { Margin, Padding } from '@mui/icons-material';
 
 export default function Student() {
     const PapperStyle = {padding:'50px 20px', width:600, margin:"20px auto"}
+    const [name,setName] =React.useState('')
+    const [address,setAddress]=React.useState('')
   return (
     <Container>
         < Paper elevation={3} style={PapperStyle}>
+        <h1>Add Student</h1>
     <Box
       component="form"
-      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      sx={{ '& > :not(style)': { m: 1} }}
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Address" variant="outlined" />
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+      value={name}
+      onChange={(e)=>setName(e.target.value)}/>
+      <TextField id="outlined-basic" label="Address" variant="outlined" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}/>
     </Box>
     </Paper> 
     </Container>
